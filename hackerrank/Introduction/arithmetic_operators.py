2c0c709dd842c75378af1ec5371bf9d799a64f35
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-03-24 00:45:20 +0100

    Create arithmetic_operators.py

---
+++ hackerrank/Introduction/arithmetic_operators.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#Author: Hack.You
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # Take sum of two integers
+    sum = a + b
+    print(sum)
+    # Take the difference between the two integers
+    diff = a - b
+    print(diff)
+    # Take the product of the two integers
+    prod = a * b 
+    print(prod)

e05c977637d479878880684e73ec637d1b051ac8
Mark Uche <113513376+markuched13@users.noreply.github.com>
2023-03-24 00:48:23 +0100

    Create division.py

---
+++ hackerrank/Introduction/division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#Author: Hack.You
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # Take division of the two integers
+    div_int = a // b
+    print(div_int)
+    # Take the division of the two integers and its result should be of a float numbers
+    div_float = a / b
+    print(div_float)
